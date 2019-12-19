2dc927c8fc79fd1636ebce95a943454426525fea
Dalio <Dalio@DALI>
2019-12-19 23:44:51 +0200

    007

---
+++ ApplicationWinforms/Groupe.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BEL;
+using DAL;
+
+namespace ApplicationWinforms
+{
+    public partial class Groupe : Form
+    {
+        public Groupe()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Accueil al = new Accueil();
+            al.ShowDialog();
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Groupes clt = GroupeDAO.Get_client_ID(int.Parse(tbID.Text));
+                tbnom.Text = clt.nom;
+                tbnbre.Text = clt.nbre.ToString();
+         
+                tbniv.Text = clt.num_niv.ToString();
+                List<Groupes> L = new List<Groupes>();
+                L.Add(clt);
+                grid_clients.DataSource = L;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Ajouter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GroupeDAO.Insert_client(tbnom.Text, int.Parse(tbnbre.Text), int.Parse(tbniv.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void modifier_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GroupeDAO.Update_client(int.Parse(tbID.Text), tbnom.Text, int.Parse(tbnbre.Text), int.Parse(tbniv.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Groupes> ListClients = GroupeDAO.Get_clients(int.Parse(tbnivlist.Text));
+                grid_clients.DataSource = ListClients;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GroupeDAO.Delete_client(int.Parse(txt_ID.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ ApplicationWinforms/Seance.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BEL;
+using DAL;
+
+namespace ApplicationWinforms
+{
+    public partial class Seance : Form
+    {
+        public Seance()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Accueil al = new Accueil();
+            al.ShowDialog();
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Seances clt = SeanceDAO.Get_client_ID(int.Parse(tbID.Text));
+                tbhdb.Text = clt.h_debut;
+                tbhfin.Text = clt.h_fin;
+                tbpf.Text = clt.nom_prof;
+                tbgr.Text = clt.num_grp.ToString();
+                tb_id_m.Text = clt.nom_matiere;
+                tbjour.Text = clt.jour;
+                tbsalle.Text = clt.n_salle;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Ajouter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SeanceDAO.Insert_client(tbhdb.Text, tbhfin.Text, tbpf.Text, int.Parse(tbgr.Text), tb_id_m.Text, tbjour.Text, tbsalle.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void modifier_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SeanceDAO.Update_client(int.Parse(tbID.Text), tbhdb.Text, tbhfin.Text, tbpf.Text, int.Parse(tbgr.Text), tb_id_m.Text, tbjour.Text, tbsalle.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SeanceDAO.Delete_client(int.Parse(txt_ID.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void tbprenom_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Seance_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
