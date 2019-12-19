37d7921e6e2bb2335e6ba2111f16e356b87dfdec
Dalio <Dalio@DALI>
2019-12-19 23:25:04 +0200

    niveauuuuuuuu

---
+++ ApplicationWinforms/Niveau.designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ApplicationWinforms
+{
+    partial class Niveau
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Niveau));
+            this.modifier = new System.Windows.Forms.Button();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbngr = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbnom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // modifier
+            // 
+            this.modifier.Location = new System.Drawing.Point(162, 171);
+            this.modifier.Name = "modifier";
+            this.modifier.Size = new System.Drawing.Size(133, 35);
+            this.modifier.TabIndex = 39;
+            this.modifier.Text = "Modifier";
+            this.modifier.UseVisualStyleBackColor = true;
+            this.modifier.Click += new System.EventHandler(this.modifier_Click);
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(215, 78);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(169, 20);
+            this.tbID.TabIndex = 33;
+            // 
+            // tbngr
+            // 
+            this.tbngr.Location = new System.Drawing.Point(215, 130);
+            this.tbngr.Name = "tbngr";
+            this.tbngr.Size = new System.Drawing.Size(169, 20);
+            this.tbngr.TabIndex = 35;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(159, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "N° Groupe";
+            // 
+            // tbnom
+            // 
+            this.tbnom.Location = new System.Drawing.Point(215, 104);
+            this.tbnom.Name = "tbnom";
+            this.tbnom.Size = new System.Drawing.Size(169, 20);
+            this.tbnom.TabIndex = 34;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(159, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Nom";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(159, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "ID";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(310, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 36);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "Consulter";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(12, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(37, 36);
+            this.button4.TabIndex = 31;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Niveau
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(671, 347);
+            this.Controls.Add(this.modifier);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.tbngr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbnom);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Name = "Niveau";
+            this.Text = "Niveau";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button modifier;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbngr;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbnom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
